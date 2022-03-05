c313c2f4a4e8950e3c599e0920c1e6dfab1cd961
Evan McCauley <evan7.mccauley@gmail.com>
2022-03-05 16:43:09 -0600

    Update GameRenderer.cs

--- Assets/Scripts/Game/GameRenderer.cs
+++ Assets/Scripts/Game/GameRenderer.cs
@@ -38,17 +38,20 @@ public class GameRenderer : MonoBehaviour
             {
                 var _entity = i < entityObjs.Count ? entityObjs[i] : Instantiate(character);
                 _entity.transform.position = new Vector3(entity.x, 0, entity.y);
-                if (i - 1 < entityObjs.Count)
+                if (i >= entityObjs.Count)
                     entityObjs.Add(_entity);
+
+                Debug.Log(_entity.transform.position);
             }
             else if (entity.tags.Contains("Food"))
             {
                 var _entity = i < entityObjs.Count ? entityObjs[i] : Instantiate(food);
                 _entity.transform.position = new Vector3(entity.x, 0, entity.y);
-                if (i - 1 < entityObjs.Count)
+                if (i >= entityObjs.Count)
                     entityObjs.Add(_entity);
+
+                Debug.Log(_entity.transform.position);
             }
-            Debug.Log(entityObjs.ToString());
         }
     }
 }
